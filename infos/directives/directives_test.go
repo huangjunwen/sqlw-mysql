06cf677a43ac7012ca2ee25935c8ae961f274143
jayven <kassarar@gmail.com>
2018-06-03 21:38:17 +0800

    Add some test for wildcard directive

---
+++ infos/directives/directives_test.go
@@ -0,0 +1,40 @@
+package directives
+
+import (
+	"context"
+	"log"
+	"testing"
+
+	"github.com/huangjunwen/sqlw-mysql/datasrc"
+	"github.com/huangjunwen/sqlw-mysql/testutils"
+)
+
+var (
+	loader *datasrc.Loader
+)
+
+func exec(query string, args ...interface{}) {
+	_, err := loader.Conn().ExecContext(context.Background(), query, args...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestMain(m *testing.M) {
+
+	err := testutils.Chain{
+		testutils.WithTstMySQLServer,
+		testutils.WithTstLoader,
+	}.Then(func(ctx context.Context) error {
+
+		loader = testutils.TstLoader(ctx)
+		m.Run()
+		return nil
+
+	})(context.Background())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
---
+++ infos/directives/wc_test.go
@@ -0,0 +1,131 @@
+package directives
+
+import (
+	"testing"
+
+	"github.com/beevik/etree"
+	"github.com/huangjunwen/sqlw-mysql/infos"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWildcardsInfo(t *testing.T) {
+
+	assert := assert.New(t)
+	_ = assert
+
+	exec("CREATE DATABASE test_wildcardsinfo")
+	exec("USE test_wildcardsinfo")
+	defer func() {
+		exec("DROP DATABASE test_wildcardsinfo")
+	}()
+
+	exec(`
+		CREATE TABLE user (
+			id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+			name VARCHAR(32) NOT NULL,
+			female BOOL,
+			KEY ix_name (name)
+		)
+	`)
+
+	exec(`
+		CREATE TABLE employee (
+			id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+			user_id INT UNSIGNED NOT NULL,
+			title VARCHAR(32) NOT NULL,
+			superior_id INT UNSIGNED,
+			KEY ix_title (title),
+			FOREIGN KEY fk_user_id (user_id) REFERENCES user (id),
+			FOREIGN KEY fk_superior_id (superior_id) REFERENCES employee (id)
+		)
+	`)
+
+	db, err := infos.NewDBInfo(loader)
+	assert.NoError(err)
+
+	{
+		doc := etree.NewDocument()
+		assert.NoError(doc.ReadFromString(`
+			<stmt name="xxx">
+				SELECT * FROM user
+			</stmt>
+		`))
+		stmt, err := infos.NewStmtInfo(loader, db, doc.Root())
+		assert.NoError(err)
+
+		wildcards := ExtractWildcardsInfo(stmt)
+		assert.False(wildcards.Valid())
+	}
+
+	{
+		doc := etree.NewDocument()
+		assert.NoError(doc.ReadFromString(`
+			<stmt name="xxx">
+				SELECT <wc table="user" /> FROM user
+			</stmt>
+		`))
+		stmt, err := infos.NewStmtInfo(loader, db, doc.Root())
+		assert.NoError(err)
+		wildcards := ExtractWildcardsInfo(stmt)
+
+		assert.True(wildcards.Valid())
+		assert.Len(wildcards.Wildcards(), 1)
+		assert.Equal(0, wildcards.Wildcards()[0].Offset())
+
+	}
+	{
+		doc := etree.NewDocument()
+		assert.NoError(doc.ReadFromString(`
+			<stmt name="xxx">
+				SELECT u.*, NOW(), u.* FROM (SELECT <wc table="user" /> FROM user) u
+			</stmt>
+		`))
+		stmt, err := infos.NewStmtInfo(loader, db, doc.Root())
+		assert.NoError(err)
+		wildcards := ExtractWildcardsInfo(stmt)
+
+		assert.True(wildcards.Valid())
+		assert.Len(wildcards.Wildcards(), 2)
+
+		assert.Equal(0, wildcards.Wildcards()[0].Offset())
+		assert.Equal("user", wildcards.Wildcards()[0].WildcardName())
+		assert.Equal(4, wildcards.Wildcards()[1].Offset())
+		assert.Equal("user", wildcards.Wildcards()[1].WildcardName())
+
+		assert.Len(stmt.QueryResultCols(), 7)
+		assert.Equal("id", wildcards.WildcardColumn(0).ColumnName())
+		assert.Equal("name", wildcards.WildcardColumn(1).ColumnName())
+		assert.Equal("female", wildcards.WildcardColumn(2).ColumnName())
+		assert.False(wildcards.WildcardColumn(3).Valid())
+		assert.Equal("id", wildcards.WildcardColumn(4).ColumnName())
+		assert.Equal("name", wildcards.WildcardColumn(5).ColumnName())
+		assert.Equal("female", wildcards.WildcardColumn(6).ColumnName())
+	}
+
+	{
+		doc := etree.NewDocument()
+		assert.NoError(doc.ReadFromString(`
+			<stmt name="Topdogs">
+			SELECT
+				<wc table="employee" as="e1" />, <wc table="user" as="u" />
+			FROM
+				employee e1
+					LEFT JOIN employee e2 ON e1.superior_id = e2.id
+					JOIN user u ON e1.user_id = u.id
+			WHERE
+				e2.id IS NULL
+			</stmt>
+		`))
+		stmt, err := infos.NewStmtInfo(loader, db, doc.Root())
+		assert.NoError(err)
+		wildcards := ExtractWildcardsInfo(stmt)
+
+		assert.True(wildcards.Valid())
+		assert.Len(wildcards.Wildcards(), 2)
+
+		assert.Equal(0, wildcards.Wildcards()[0].Offset())
+		assert.Equal("e1", wildcards.Wildcards()[0].WildcardName())
+		assert.Equal(4, wildcards.Wildcards()[1].Offset())
+		assert.Equal("u", wildcards.Wildcards()[1].WildcardName())
+	}
+}
